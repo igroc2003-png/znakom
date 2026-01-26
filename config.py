7a8a84dc75631d4d558417ee84d3a1abd9e8dc6c
igroc2003-png <igroc2003@gmail.com>
2026-01-26 12:29:21 +0700

    config.py

--- config.py
+++ config.py
@@ -1,4 +1,1 @@
-# Токен бота MAX
-TOKEN = "f9LHodD0cOJTtjGz_nLQlHs2b6j_MMBp1Kdb6egSNRFohzFBxRrN_C3k1Q-8NCcr5xnKBfdIyhIaJRhrWmnS"
-DB_PATH = "profiles.db"
-GEO_DB = "geo.db"
+TOKEN = os.getenv("BOT_TOKEN")
