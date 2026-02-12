b974137ec6d4a2ac5c6198df19568e906ed362e0
igroc2003-png <igroc2003@gmail.com>
2026-02-12 14:44:10 +0700

    config.py

--- config.py
+++ config.py
@@ -2,3 +2,24 @@ import os
 
 TOKEN = os.getenv("TOKEN")  # теперь работает
 
+# ================== DATABASE ==================
+DB_PATH = "profiles.db"
+GEO_DB = "geo.db"
+
+
+# ID Max
+ADMIN_ID = 200686820
+SUPPORT_URL = "https://max.ru/u/f9LHodD0cOKiKzr7C6wGuyRkZ-dyLuqHqXoQ1IxbhnV0yhJ9DLfhXrjZIfw"
+
+
+
+
+# Настройки Robokassa
+ROBO_MERCHANT_LOGIN = "ipmav"
+ROBO_PASS1 = "Qazwsxedc1"
+ROBO_PASS2 = "Qazwsxedc2"
+ROBO_TEST = 1  # 1 для тестирования, 0 для реального использования
+
+
+
+
