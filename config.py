f0300f4cc64926c5fb15ae6f85872b6979889cc8
igroc2003-png <igroc2003@gmail.com>
2026-02-21 00:48:33 +0700

    config.py

--- config.py
+++ config.py
@@ -1,15 +1,12 @@
 import os
 TOKEN = os.getenv("BOT_TOKEN")
-# ================== DATABASE ==================
-DB_PATH = "profiles.db"
-GEO_DB = "geo.db"
-
 
 # ID Max
 ADMIN_ID = 198191393
 SUPPORT_URL = "https://max.ru/u/f9LHodD0cOKiKzr7C6wGuyRkZ-dyLuqHqXoQ1IxbhnV0yhJ9DLfhXrjZIfw"
 
-
+#Бот
+BOT_USERNAME = "id420105283818_1_bot"
 
 
 # Настройки IntellectMoney
@@ -21,13 +18,6 @@ IM_TEST = True   # пока тест
 # Настройки yookassa.ru
 YOOKASSA_SHOP_ID = "1262924"                                                  # ID магазина
 YOOKASSA_SECRET_KEY = "live_vhNMjcNeJhnHG0HcBFjCXV1c97DM3Wbgi1Eg-ZHyLmA"      # секретный ключ API
-
-
-# Настройки Robokassa
-ROBO_MERCHANT_LOGIN = "ipmav"
-ROBO_PASS1 = "Qazwsxedc1"
-ROBO_PASS2 = "Qazwsxedc2"
-ROBO_TEST = 1  # 1 для тестирования, 0 для реального использования
 
 
 
